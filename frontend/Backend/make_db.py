b362b9b26cdded139ff807ab6db9c0555afa1322
Christopher Vu <107224696+ChrisMV2007@users.noreply.github.com>
2023-04-10 16:50:23 -0700

    Delete frontend directory

--- frontend/Backend/make_db.py
+++
@@ -1,8 +0,0 @@
-# For ease of testing
-import sqlite3
-
-conn = sqlite3.connect('frontend/Backend/userdata.db')
-c = conn.cursor()
-c.execute("CREATE TABLE userdata (email TEXT,password TEXT);")
-conn.commit()
-conn.close()
